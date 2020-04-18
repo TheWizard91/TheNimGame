8560bff5ee9c85af66391a469d2cf5b630555273
TheWizard91 <agiapong@gmail.com>
2020-04-18 17:54:47 -0400

    Updates on AI

--- Computer.py
+++ Computer.py
@@ -4,18 +4,30 @@ class ArtificialIntelligence():
         pass
     def _goBinary(self):
         reminder=0
-        exponent=0
+        exponent=4
+        l=[]
         for i in self._piles.values():
-            for y in range(0,4):
+            # print("in first iteration "+str(i))
+            for y in range(0,5):
                 if (i>=2**exponent):
-                    reminder=i-2**exponent
+                    print("in first if-----------------")
+                    print("i: "+str(i))
+                    # print("real exposnent:"+str(exponent))
+                    e=2**exponent
+                    # print("exponent number: "+str(e))
+                    reminder=i-(e)
                     i=reminder
-                    print(reminder)
                     exponent-=1
-                if exponent==0:
+                if (exponent==0):
+                    print("in second if---------------------------")
+                    print("i: "+str(i))
+                    # print("real exposnent:"+str(exponent))
+                    # print("exponent number: "+str(e))
                     exponent=4
-                
-            # print(i)
+                # print("in second iteration "+str(i))  
+                exponent-=1
+                l.append(i)
+        print(l)
 
     def winningStrategy(self):
         self._goBinary()
