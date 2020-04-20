ecbaecdf25e2c8319f3639a0a67a902824b73cec
TheWizard91 <agiapong@gmail.com>
2020-04-20 17:15:54 -0400

    first commit

--- Computer.py
+++ Computer.py
@@ -1,38 +1,46 @@
 class ArtificialIntelligence():
     def __init__(self, p):
+        """Computer playing the game agianst player1
+        pre: p dictionary of piles and their values.
+        post:---"""
+
         self._piles=p
+        print(self._piles, self._piles.values())
         pass
+
     def _goBinary(self):
+        """Get the binary representation of values in each pile"""
+        c=0
         reminder=0
         exponent=4
-        l=[]
+        twoRaiseToexponent=2**exponent
+        dictionaryOfBaseRasedToPowers={"16":0,"8":0,"4":0,"2":0,"1":0}
+        # tuppleOfBaseRasedToPowers=(16,8,4,2,1)
+        
         for i in self._piles.values():
-            # print("in first iteration "+str(i))
             for y in range(0,5):
-                if (i>=2**exponent):
-                    print("in first if-----------------")
-                    print("i: "+str(i))
-                    # print("real exposnent:"+str(exponent))
-                    e=2**exponent
-                    # print("exponent number: "+str(e))
-                    reminder=i-(e)
+                if (i>=twoRaiseToexponent):
+                    # valueInTupple=tuppleOfBaseRasedToPowers[y]*NOT NEEDED
+                    dictionaryOfBaseRasedToPowers[str(twoRaiseToexponent)]+=1
+                    # print(dictionaryOfBaseRasedToPowers[str(valueInTupple)])
+                    # print(i)
+                    reminder=i-(twoRaiseToexponent)
                     i=reminder
-                    exponent-=1
                 if (exponent==0):
-                    print("in second if---------------------------")
-                    print("i: "+str(i))
-                    # print("real exposnent:"+str(exponent))
-                    # print("exponent number: "+str(e))
-                    exponent=4
-                # print("in second iteration "+str(i))  
+                    exponent=5
+                c+=1
                 exponent-=1
-                l.append(i)
-        print(l)
+                twoRaiseToexponent=2**exponent
+                print("c: "+str(c))
 
+        print(dictionaryOfBaseRasedToPowers)
+        # print(self._piles)
     def winningStrategy(self):
+        """Strategy to which the computer will beat the opponent."""
+        
         self._goBinary()
         pass
     # this is now a rest
 if __name__=="__main__":
-    ai=ArtificialIntelligence({"A":19,"B":11,"C":1})
+    ai=ArtificialIntelligence({"A":19,"B":11,"C":1})#, "D":15, "E":10, "G":5})
     ai._goBinary()
